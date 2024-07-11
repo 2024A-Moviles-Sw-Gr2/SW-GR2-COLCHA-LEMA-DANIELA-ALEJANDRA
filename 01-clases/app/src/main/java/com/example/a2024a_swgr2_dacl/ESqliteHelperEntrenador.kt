4110342c904d6fac9d1277b04f3ff7755a047f43
dacol <da.colcha@hotmail.com>
2024-07-11 07:34:02 -0500

    Change clase 16

--- 01-clases/app/src/main/java/com/example/a2024a_swgr2_dacl/ESqliteHelperEntrenador.kt
+++ 01-clases/app/src/main/java/com/example/a2024a_swgr2_dacl/ESqliteHelperEntrenador.kt
@@ -43,4 +43,37 @@ class ESqliteHelperEntrenador(
         basedatosEscritura.close()
         return if(resultadoGuardar.toInt() == -1) false else true
     }
+
+    fun eliminarEntrenadorFormulario(id:Int): Boolean{
+        val conexionEscritura = writableDatabase
+
+        //Consulta SQL para eliminar
+        val parametrosConsultaDelete = arrayOf(id.toString())
+        val resultadoEliminar = conexionEscritura.delete(
+            "ENTRENADOR",
+            "id=?",
+            parametrosConsultaDelete
+        )
+        conexionEscritura.close()
+        return if(resultadoEliminar.toInt() == -1) false else true
+    }
+
+    fun actualizarEntrenadorFormulario(
+        nombre: String, descripcion: String, id: Int
+    ):Boolean{
+        val conexionEscritura = writableDatabase
+        val valoresActualizar = ContentValues()
+        valoresActualizar.put("nombre", nombre)
+        valoresActualizar.put("descripcion", descripcion)
+
+        val parametrosActualizar = arrayOf(id.toString())
+        val resultadosAtualizacion = conexionEscritura.update(
+            "ENTRENADOR",
+            valoresActualizar,
+            "id=?",
+            parametrosActualizar
+        )
+        conexionEscritura.close()
+        return if(resultadosAtualizacion.toInt()==-1) false else true
+    }
 }
